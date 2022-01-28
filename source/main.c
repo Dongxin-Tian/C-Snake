9f7481b6ed0a6c9990fcdf51459f8b4337376051
Tian, Dongxin <70252050+Dongxin-Tian@users.noreply.github.com>
2022-01-27 21:50:34 -0500

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,19 +1,59 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <conio.h>
+#include <stdbool.h>
+
+char input_c;
+
+// Grid
+int length, width;
+char grid[20][20];
+
+void print_title_screen() {
+    char title_name[270] = " ******    ***   **     *****     **   **    *******\n**         ****  **    **   **    **  **     **     \n *****     ** ** **    *******    *****      *****  \n     **    **  ****    **   **    **  **     **     \n******     **   ***    **   **    **   **    *******\n";
+    printf("%s", title_name);
+}
+
+/*
+ * Input for gameplay
+ */
+void check_input() {
+    if (kbhit()) {
+        input_c = getch();
+        //printf("%c is pressed", input_c);
+    }
+}
+
+void title_loop() {
+    for (;;) {
+        print_title_screen();
+
+        // Get menu selection input
+        if (getch() == '\033') {
+            switch (getch()) {
+                case 'A': // Up
+
+                    break;
+                case 'B': // Down
+
+                    break;
+            }
+        }
+    }
+}
 
 int main(void) {
+    
+    bool is_quitted = false;
+    while (!is_quitted) {
 
-    char c;
-    while (1) {
-        if (kbhit()) {
-            c = getch();
+    }
+    title_loop();
 
-            if ((int)c == 27)
-                break;
-            
-            printf("%c is pressed", c);
-        }
+    // Game loop
+    for (;;) {
+        check_input();
     }
 
     return 0;
