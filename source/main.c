f775d8b2b1998d9b8914896288e4aac92ef31da0
Tian, Dongxin <70252050+Dongxin-Tian@users.noreply.github.com>
2022-01-28 18:27:30 -0500

    Update main.c

--- source/main.c
+++ source/main.c
@@ -4,13 +4,27 @@
 #include <conio.h>
 #include <stdbool.h>
 
+typedef struct {
+    int x, y;
+} Body;
+
 char input_c;
 
 // Grid
-int length, width;
+int length = 15, width = 10;
 char grid[20][20];
 
-void print_title_screen() {
+// Menu
+int selection_index = 0;
+bool is_quitted = false;
+
+// Define functions
+void print_board(void);
+
+/*
+ * Print the title "SNAKE"
+ */
+void print_title_screen(void) {
     char title_name[270] = " ******    ***   **     *****     **   **    *******\n**         ****  **    **   **    **  **     **     \n *****     ** ** **    *******    *****      *****  \n     **    **  ****    **   **    **  **     **     \n******     **   ***    **   **    **   **    *******\n";
     printf("%s", title_name);
 }
@@ -18,43 +32,132 @@ void print_title_screen() {
 /*
  * Input for gameplay
  */
-void check_input() {
+void check_input(void) {
     if (kbhit()) {
         input_c = getch();
-        //printf("%c is pressed", input_c);
+        printf("%c is pressed", input_c);
     }
 }
 
-void title_loop() {
+/*
+ * The title loop for menu selecting
+ */
+void title_loop(void) {
+    char selections[3][10] = { "Start", "Option", "Quit" };
+    bool title_exitted = false;
+
     for (;;) {
+
         print_title_screen();
 
-        // Get menu selection input
-        if (getch() == '\033') {
-            switch (getch()) {
-                case 'A': // Up
-
-                    break;
-                case 'B': // Down
-
-                    break;
-            }
+        // Print selections
+        for (int i = 0; i < 3; i++) {
+            char c = ' ';
+            if (selection_index == i)
+                c = '>';
+            printf("%c %s\n", c, selections[i]);
         }
+
+        // Get menu selection input
+        switch (getch()) {
+            case 224: // Arrow
+                switch (getch()) {
+                    case 72: // Up
+                        selection_index--;
+                        if (selection_index < 0)
+                            selection_index = 2;
+                        break;
+
+                    case 80: // Down
+                        selection_index++;
+                        if (selection_index > 2)
+                            selection_index = 0;
+                        break;
+                }
+                break;
+
+            case 13: // Enter
+                // Main menu selection
+                switch (selection_index) {
+                    case 0: // Start
+                        title_exitted = true;
+                        break;
+
+                    case 1: // Option
+                        system("cls");
+                        break;
+
+                    case 2: // Quit
+                        title_exitted = true;
+                        is_quitted = true;
+                        break;
+                }
+                break;
+        }
+
+        if (title_exitted) break;
+
+        system("cls");
     }
+}
+
+/*
+ * The option selecting loop
+ * TODO: Finish with file I/O
+ */
+void option_selecting(void) {
+
+}
+
+/*
+ * Game loop
+ */
+void game_loop(void) {
+    for (;;) {
+        print_board();
+
+        //check_input();
+
+        sleep(1);
+
+        system("cls");
+    }
+}
+
+/*
+ * Print board
+ */
+void print_board(void) {
+    char board_str[length * (width + 1)];
+    int str_index = 0;
+    for (register int i = 0; i < width; i++) {
+        for (register int j = 0; j < length; j++) {
+            board_str[str_index] = grid[i][j];
+            str_index++;
+            printf("%c", grid[i][j]);
+        }
+        printf("\n");
+    }
+    //printf(board_str);
 }
 
 int main(void) {
     
-    bool is_quitted = false;
-    while (!is_quitted) {
-
+    // Initialize board
+    for (register int i = 0; i < width; i++) {
+        for (register int j = 0; j < length; j++) {
+            grid[i][j] = 'O';
+        }
     }
-    title_loop();
 
-    // Game loop
     for (;;) {
-        check_input();
-    }
+        // Title loop
+        title_loop();
+        if (is_quitted) return 0;
 
+        // Game loop
+        game_loop();
+    }
+    
     return 0;
 }
